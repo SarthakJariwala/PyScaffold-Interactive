84ae321524521bc36c481bbde77d97ad1bccc942
Sarthak Jariwala <jariwala@uw.edu>
2020-06-29 00:53:12 -0700

    first interactive putup command

---
+++ src/pyscaffold_interactive/cli.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+This is a skeleton file that can serve as a starting point for a Python
+console script. To run this script uncomment the following lines in the
+[options.entry_points] section in setup.cfg:
+
+    console_scripts =
+         fibonacci = pyscaffold_interactive.skeleton:run
+
+Then run `python setup.py install` which will install the command `fibonacci`
+inside your current environment.
+Besides console scripts, the header (i.e. until _logger...) of this file can
+also be used as template for Python modules.
+
+Note: This skeleton file can be safely removed if not needed!
+"""
+
+import argparse
+import sys
+import logging
+
+import click
+from pyscaffold import templates
+from pyscaffold.api import create_project
+from pyscaffold.extenstions.tox import Tox
+from pyscaffold.extenstions.travis import Travis
+from pyscaffold.extenstions.namespace import Namespace
+
+from pyscaffold_interactive import __version__
+
+__author__ = "Sarthak Jariwala"
+__copyright__ = "Sarthak Jariwala"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+license_choices = templates.licenses.keys()
+extensions = []
+
+def main():
+  """Interactive Python project template setup
+  """
+
+  project_name = click.prompt(
+    click.style("Enter your project name ", fg="blue"),
+    default="PyProject"
+  )
+
+  package_name = click.prompt(
+    click.style("Enter your package name ", fg="blue"),
+    default=f"{project_name}"
+  )
+
+  description = click.prompt(
+    click.style("Enter your package description ", fg="blue"),
+    default="PyScaffold-Interactive is an interactive tool to easily\n"+
+    "put up the scaffold of a Python project."
+  )
+
+  license = click.prompt(
+    click.style("Choose License", fg="blue"),
+    show_choices=True,
+    type=click.Choice(license_choices, case_sensitive=True),
+    default='mit'
+  )
+
+  make_tox = click.prompt(
+    click.style("Generate config files for automated testing using tox? ", fg='blue'),
+    show_choices=True,
+    type=click.Choice(['y','n'], case_sensitive=False),
+    default='y'
+  ).lower()
+
+  if make_tox == 'y':
+    extensions.append(Tox('tox'))
+
+  create_travis = click.prompt(
+    click.style("Generate config and script files for Travis CI.? ", fg='blue'),
+    show_choices=True,
+    type=click.Choice(['y','n'], case_sensitive=False),
+    default='y'
+  ).lower()
+
+  if create_travis == 'y':
+    extensions.append(Travis('travis'))
+
+  create_project(
+    project=project_name,
+    description=description,
+    license=license,
+    extensions=extensions
+  )
+
+
+
+
+def run():
+    """Entry point for console_scripts
+    """
+    main()
+
+
+if __name__ == "__main__":
+    run()
