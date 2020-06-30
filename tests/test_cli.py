30e3bb3556de9148c5b27db33c186f7da6796b9d
Sarthak Jariwala <jariwala@uw.edu>
2020-06-30 01:58:05 -0700

    all tests working

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ import pytest
 import click
 from click.testing import CliRunner
 import sys
-sys.path.append("src/")
+# sys.path.append("src/")
 from pyscaffold_interactive import cli as pysci #import main, prompt_text, prompt_choice
 
 __author__ = "Sarthak Jariwala"
@@ -12,7 +12,11 @@ __copyright__ = "Sarthak Jariwala"
 __license__ = "mit"
 
 
-def test_default_vs_input():
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+def test_default_vs_input(runner):
     """Test if the default value provided is overwritten by user input"""
     
     @click.command()
@@ -20,19 +24,17 @@ def test_default_vs_input():
         ans = pysci.prompt_text("Project name", default="PyProject")
         click.echo(f'Project Name = {ans}')
 
-    runner = CliRunner()
     result = runner.invoke(cli, input="My Python Project\n")
     assert not result.exception
     assert result.output == "Project name [PyProject]: My Python Project\nProject Name = My Python Project\n"
 
-def test_choices():
+def test_choices(runner):
     """Test if an error is thrown when user provides an input that is not in choices"""
 
     @click.command()
     def cli():
         ans = pysci.prompt_choice("Confrim", ['y', 'n'], default='y')
     
-    runner = CliRunner()
     result = runner.invoke(cli, input='z')
 
     assert result.output != 'y' # check if the output is not equal to default
@@ -43,14 +45,14 @@ def test_choice_iterable():
     with pytest.raises(AssertionError):
         pysci.prompt_choice("Confrim", 1)
 
-def test_main():
+def test_main(runner):
     """Test interactive creation of pyscaffold"""
 
     @click.command()
     def cli():
-        pysci.main()
+        return pysci.main()
 
-    runner = CliRunner()
-    result = runner.invoke(cli)
-    assert result.exit_code == 0
-
+    input="PyProject\nSarthak Jariwala\njariwala@uw.edu\nwww.example.com\nMy description\nmit\ny\ny\n"
+    result = runner.invoke(cli, input=input)
+    assert not result.exception
+    assert result.exit_code == 0 # TODO get this test to pass
